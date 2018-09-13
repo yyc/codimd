ca379c2e70c8d4f2856eccf0198596ec1dccce50
Yuchuan <yyc478@gmail.com>
2018-09-13 16:00:50 +0800

    add logging to source

--- public/js/source.js
+++ public/js/source.js
@@ -1,19 +1,50 @@
-import {
-  createContext,
-  createSlangContext,
-  runInContext
-} from 'js-slang'
+import { createContext, createSlangContext, runInContext } from "js-slang";
 
-import { toString } from 'js-slang/dist/interop'
+import { toString } from "js-slang/dist/interop";
 
-import { Value } from 'js-slang/dist/types'
+import { Value } from "js-slang/dist/types";
 
 window.source_runtime = {
   codeMap: {},
   codeIndex: 0,
-}
+  displays: []
+};
 
-window.source_runtime.context =  createContext(3);
+window.source_runtime.context = (() => {
+  // Define our builtins as in cadet-frontend/src/utils/slangHelper
+  function display(t) {
+    window.source_runtime.displays.push(toString(t));
+  }
+  display.__SOURCE__ = "display(a)";
+
+  function cadetPrompt(any) {
+    return prompt(toString(value))
+  }
+  cadetPrompt.__SOURCE__ = 'prompt(a)'
+
+  function cadetAlert(value) {
+    alert(toString(value))
+  }
+  cadetAlert.__SOURCE__ = 'alert(a)'
+
+  function visualiseList(list) {
+    if (window.ListVisualizer) {
+      window.ListVisualizer.draw(list)
+    } else {
+      return list;
+    }
+  }
+  visualiseList.__SOURCE__ = 'draw_list(a)'
+
+  const externalBuiltIns = {
+    display,
+    prompt: cadetPrompt,
+    alert: cadetAlert,
+    visualiseList
+  }
+
+   return createContext(3, [], null, externalBuiltIns);
+})()
 
 export function clearCodeMap() {
   window.source_runtime.codeMap = {};
@@ -25,23 +56,26 @@ export function registerCode(code) {
   return window.source_runtime.codeIndex++;
 }
 
-export function execIndex(index)  {
-  const code = window.source_runtime.codeMap[index];
+export function execButton(codeIndex, button) {
+  const code = window.source_runtime.codeMap[codeIndex];
+  const result = runInContext(
+    code,
+    window.source_runtime.context
+  ).then(result => {
+    if (result.status == "error") {
+      console.log(window.source_runtime.context.errors);
+    } else if (result.status == "finished") {
+      console.log("logged", window.source_runtime.displays);
+      console.log("finished running with value", result.value);
+    }
+  });
 }
-window.execIndex = execIndex;
 
 export function addCodeHandlers() {
-  $('.exec_button').each(function(index, button) {
-    const codeIndex = $(button).attr('data-code-index');
+  $(".exec_button").each(function(index, button) {
+    const codeIndex = $(button).attr("data-code-index");
     $(button).click(function(e) {
-      const code = window.source_runtime.codeMap[codeIndex];
-      runInContext(code, window.source_runtime.context).then(result => {
-        if(result.status == 'error') {
-          console.log(window.source_runtime.context.errors);
-        } else if(result.status == 'finished') {
-          console.log('finished running with value', result.value);
-        }
-      })
-    })
+      execButton(codeIndex, button);
+    });
   });
 }
